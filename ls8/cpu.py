dd9d4e93cac15f67cadf16d3ec4796bf5ee4eed5
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-21 20:27:27 -0400

    Finished day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,40 +10,55 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.register = [0] * 8
+        self.reg = [0] * 8
         self.pc = 0
-        self.instructions = {0b10000010: "LDI",
-                             0b01000111: "PRN",
-                             0b00000001: "HLT"}
+        self.instructions = {
+            0b10000010 : self.ldi,
+            0b01000111 : self.prn,
+            0b10100010 : self.alu,
+            HLT : self.hlt,
+        }
 
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
+        program = []
+
+        with open(sys.argv[1], 'r') as reader:
+            line = reader.readline()
+            while line != "":
+                words = line.split(" ")
+                num_str = f"0b{words[0]}"
+                num = int(num_str, 2)
+                program.append(num)
+                line = reader.readline()
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
 
-    def alu(self, op, reg_a, reg_b):
+    def alu(self, reg_a, reg_b, op):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        if op == 0b10100010:
+            self.ram[reg_a] *= self.ram[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -76,21 +91,10 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if ir == 0b10000010:
-                self.ram_write(operand_b, operand_a)
-                # self.pc += 3
-            elif ir == 0b01000111:
-                print(f"{self.ram_read(operand_a)}")
-                # self.pc += 2
-            elif ir == HLT:
-                break
+            self.instructions[ir](operand_a, operand_b, ir)
 
             instruct = ((ir & 0b11000000) >> 6) + 1
             self.pc += instruct
-
-            # num_params = int(instruct[2:4])
-            # print(num_params)
-            # self.pc +=
 
 
     def ram_read(self, address):
@@ -99,6 +103,13 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
-ls8 = CPU()
-ls8.load()
-ls8.run()
+    def ldi(self, address, value, *args):
+        self.ram_write(value, address)
+
+    def prn(self, address, *args):
+        print(f"{self.ram_read(address)}")
+
+    def hlt(self, *args):
+        sys.exit()
+
+
