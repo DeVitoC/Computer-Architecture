9b037d79175a7133e42e455b79dc42d4554d7e65
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-24 13:35:01 -0400

    Completed sprint challenge + extra ALU operations AND, OR, XOR, NOT, SHL, SHR, and MOD.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,17 +13,43 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.fl = 0 # LGE
         self.reg[7] = 0xF4
         self.instructions = {
-            0b10000010: self.ldi,
-            0b01000111: self.prn,
-            0b10100010: self.alu,
-            0b10100000: self.alu,
-            HLT: self.hlt,
-            0b01000101: self.push,
-            0b01000110: self.pop,
-            0b01010000: self.call,
-            0b00010001: self.ret,
+            0b10000010: self.ldi, # Load
+            0b01000111: self.prn, # Print
+            0b10100010: self.alu, # Multiply
+            0b10100000: self.alu, # ADD
+            0b10101000: self.alu, # AND
+            0b01100110: self.alu, # Decrement
+            0b10100011: self.alu, # Divide
+            0b01100101: self.alu, # Increment
+            0b10100100: self.alu, # Modulo (division remainder)
+            0b01101001: self.alu, # NOT binary operation
+            0b10101010: self.alu, # OR binary operation
+            0b10101100: self.alu, # Shift Left
+            0b10101101: self.alu, # Shift Right
+            0b10100001: self.alu, # Subtract
+            0b10101011: self.alu, # Binary XOR
+            HLT: self.hlt, # Halt
+            0b01000101: self.push, # Push to stack
+            0b01000110: self.pop, # Pop from stack
+            0b01010000: self.call, # Call subroutine
+            0b00010001: self.ret, # Return
+            0b10100111: self.cmp, # Compare equal or greater/lesser
+            0b01010010: self.int, # Interrupt
+            0b00010011: self.iret, # Interrupt return
+            0b01010101: self.jeq, # Jump if equal
+            0b01011010: self.jge, # Jump if greater or equal
+            0b01010111: self.jgt, # Jump if greater than
+            0b01011001: self.jle, # Jump if less than or equal
+            0b01011000: self.jlt, # Jump if less than
+            0b01010110: self.jne, # Jump if not equal
+            0b01010100: self.jmp, # Jump
+            0b10000011: self.ld, # Load
+            0b00000000: self.nop, # No operation
+            0b01001000: self.pra, # Print alpha character
+            0b10000100: self.st, # Store contents of second register in first
         }
 
     def load(self):
@@ -36,7 +62,11 @@ class CPU:
             line = reader.readline()
             while line != "":
                 words = line.split(" ")
-                num_str = f"0b{words[0]}"
+                first_word = words[0].rstrip()
+                if not first_word.isnumeric():
+                    line = reader.readline()
+                    continue
+                num_str = f"0b{first_word}"
                 num = int(num_str, 2)
                 program.append(num)
                 line = reader.readline()
@@ -48,11 +78,35 @@ class CPU:
     def alu(self, reg_a, reg_b, op):
         """ALU operations."""
 
-        if op == 0b10100000:
+        if op == 0b10100000: # Addition
             self.reg[reg_a] += self.reg[reg_b]
-        if op == 0b10100010:
+        elif op == 0b10100010: # Multiplication
             self.ram[reg_a] *= self.ram[reg_b]
-        #elif op == "SUB": etc
+        elif op == 0b10101000: # AND Binary operation
+            self.ram[reg_a] = self.ram[reg_a] & self.ram[reg_b]
+        elif op == 0b01100110: # Decrement
+            self.ram[reg_a] -= 1
+        elif op == 0b10100011: # Divide
+            if self.ram[reg_b] == 0:
+                print("Cannot divide by 0")
+                self.hlt()
+            self.ram[reg_a] = self.ram[reg_a] // self.ram[reg_b]
+        elif op == 0b01100101: # Increment
+            self.ram[reg_a] += 1
+        elif op == 0b10100100: # Modulo
+            self.ram[reg_a] % self.ram[reg_b]
+        elif op == 0b01101001: # NOT Binary operation
+            self.ram[reg_a] = ~self.ram[reg_a]
+        elif op == 0b10101010: # OR binary operation
+            self.ram[reg_a] = self.ram[reg_a] | self.ram[reg_b]
+        elif op == 0b10101100: # Binary shift left
+            self.ram[reg_a] = self.ram[reg_a] << self.ram[reg_b]
+        elif op == 0b10101101: # Binary shift Right
+            self.ram[reg_a] = self.ram[reg_a] >> self.ram[reg_b]
+        elif op == 0b10100001: # Subtract
+            self.ram[reg_a] -= self.ram[reg_b]
+        elif op == 0b10101011: # Binary XOR
+            self.ram[reg_a] = self.ram[reg_a] ^ self.ram[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -83,10 +137,11 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            self.instructions[ir](operand_a, operand_b, ir)
-
             instruct = ((ir & 0b11000000) >> 6) + 1
             self.pc += instruct
+            self.instructions[ir](operand_a, operand_b, ir)
+
+
 
     def ram_read(self, address):
         return self.ram[address]
@@ -95,10 +150,11 @@ class CPU:
         self.ram[address] = value
 
     def ldi(self, address, value, *args):
-        self.ram_write(value, address)
+        # self.ram_write(value, address)
+        self.reg[address] = value
 
     def prn(self, address, *args):
-        print(f"{self.ram_read(address)}")
+        print(f"{self.reg[address]}")
 
     def hlt(self, *args):
         sys.exit()
@@ -112,10 +168,65 @@ class CPU:
         self.reg[7] += 1
 
     def call(self, address, *args):
-        pass
+        return_to = self.pc + 2
+        self.push(return_to)
+        self.pc = self.ram[self.pc + 1]
 
     def ret(self, address, *args):
-        pass
+        self.pop(address)
+        self.pc = self.ram[address]
 
     def jmp(self, address, *args):
-        self.pc = address
+        self.pc = self.reg[address]
+
+    def cmp(self, address1, address2, *args):
+        if self.reg[address1] == self.reg[address2]:
+            self.fl = 0b00000001
+        elif self.reg[address1] < self.reg[address2]:
+            self.fl = 0b00000100
+        elif self.reg[address1] > self.reg[address2]:
+            self.fl = 0b00000010
+
+    def int(self, address, *args):
+        pass
+
+    def iret(self, *args):
+        pass
+
+    def jeq(self, address, *args):
+        if self.fl == 0b00000001:
+            self.jmp(address)
+
+    def jge(self, address, *args):
+        if self.fl == 0b00000001 or self.fl == 0b00000010:
+            self.jmp(address)
+
+    def jgt(self, address, *args):
+        if self.fl == 0b00000010:
+            self.jmp(address)
+
+    def jle(self, address, *args):
+        if self.fl == 0b00000100 or self.fl == 0b00000001:
+            self.jmp(address)
+
+    def jlt(self, address, *args):
+        if self.fl == 0b00000100:
+            self.jmp(address)
+
+    def jne(self, address, *args):
+        if self.fl != 0b00000001:
+            self.jmp(address)
+
+    def ld(self, address1, address2, *args):
+        self.ram_write(address1, address2)
+
+    def nop(self, *args):
+        return
+
+    def pra(self, address, *args):
+        pass
+
+    def st(self, address1, address2, *args):
+        self.reg[address1] = self.reg[address2]
+
+
